b8ab76e444efff49ee11067a59de79b26d766f48
Dashadower <tttllshin@gmail.com>
2017-05-13 21:19:38 +0900

    Changed Heuristics function

--- Analyzer.py
+++ Analyzer.py
@@ -1,5 +1,5 @@
 from main import AICore
-
+from FilterStrings import Open2,Open3,Open4,Closed4
 class Analyzer():
     def __init__(self, Board, template=None):
         self.Board = Board
@@ -10,26 +10,26 @@ class Analyzer():
 
     def ThreatSpaceSearch(self):
         pass
-    def Analyze(self, StoneType):
+    def Parser(self, StoneType):
         FoundPatterns = []
         PassedStones = []
         Open3 = 0
         BlackStones = self.Board.BlackStones
         WhiteStones = self.Board.WhiteStones
+        print("Parser Start")
         if StoneType == "black":
             # Check Vertical repetitions
             for y in range(1, self.Board.size[1] + 1):
                 for x in range(1, self.Board.size[0] + 1):
                     if (x,y) not in PassedStones and (x,y) in BlackStones:
-                        if (x,y-1) not in WhiteStones and (x,y-1) not in BlackStones:
+                        if (x,y-1) not in BlackStones:
                             data = ""
-                            pos = []
                             for g in range(0,6):
                                 if (x,y-1+g) in BlackStones:
                                     data += "o"
                                     PassedStones.append((x,y-1+g))
                                 elif (x,y-1+g) in WhiteStones: data += "x"
-                                else: data += " "
+                                else: data += "-"
 
                             if data.count("o") >= 2:
                                 FoundPatterns.append(data)
@@ -38,24 +38,44 @@ class Analyzer():
             for x in range(1,self.Board.size[0]+1):
                 for y in range(1,self.Board.size[1]+1):
                     if (x,y) not in PassedStones and (x,y) in BlackStones:
-                        if (x-1,y) not in WhiteStones and (x-1,y) not in BlackStones:
+                        if (x-1,y) not in BlackStones:
                             data = ""
-                            pos = []
                             for g in range(0,6):
                                 if (x-1+g,y) in BlackStones:
                                     data += "o"
                                     PassedStones.append((x-1+g, y))
                                 elif (x-1+g,y) in WhiteStones: data += "x"
-                                else: data += " "
+                                else: data += "-"
 
                             if data.count("o") >= 2:
                                 FoundPatterns.append(data)
-
+        print(FoundPatterns)
+        print("Parser End")
         return FoundPatterns
 
-    def Lexer(self):
-        pass
-    def get_8(self, Position):
+    def Grader(self,StoneType):
+        score = 0
+        print("Grader Start")
+        print("Running Parser")
+        patterns = self.Parser(StoneType)
+        for pattern in patterns:
+            if pattern in Open2:
+                print(pattern,"Open2   +5")
+                score += 5
+            elif pattern in Open3:
+                print(pattern,"Open3   +50")
+                score += 50
+            elif pattern in Open4:
+                print(pattern,"Open4   +500")
+                score += 500
+            elif pattern in Closed4:
+                print(pattern,"Closed4 +50")
+                score += 50
+        print("Move score:",score)
+        print("End Grader")
+        return score
+
+    def Get_8(self, Position):
         data = []
         for x, y in [(Position[0] + i, Position[1] + j) for i in (-1, 0, 1) for j in (-1, 0, 1) if i != 0 or j != 0]:
             if x > self.Board.size[0] or x < 0 or y > self.Board.size[1] or y < 0:
@@ -77,9 +97,10 @@ ai.Board.AddStone("black", (6, 7))
 ai.Board.AddStone("black", (6, 8))
 ai.Board.AddStone("black", (6, 10))
 ai.Board.AddStone("black", (8, 7))
-#ai.Board.AddStone("black", (8, 8))
+# ai.Board.AddStone("black", (8, 8))
 
 ai.Board.AddStone("white", (6, 6))
-print("Black:",ai.Board.BlackStones)
+print("Black:", ai.Board.BlackStones)
 heuristics = Analyzer(ai.Board)
-print(heuristics.Analyze("black"))
+
+heuristics.Grader("black")
--- FilterStrings.py
+++ FilterStrings.py
@@ -5,28 +5,6 @@
 # x (alphabet x) means attacker's opponent's stone
 # each pattern must be 6 characters long
 # -----------------------------------------
-# Open3 is a threat in which all the ends of 3 repetitive stones are open
-# The third and fourth are also counted as Open3
-Open3 = [
-    "-ooo--",
-    "--ooo-",
-    "-o-oo-",
-    "-oo-o-"
-]
-# -----------------------------------------
-# Open4 is a threat in which all the ends of 4 repetitive stones are open
-# If the attacker does not have an Closed4 or Open4 to make into 5, Attacker is guarenteed to win next turn
-Open4 = [
-    "-oooo-"
-]
-# -----------------------------------------
-# Closed4 is a threat in which one of the ends of 4 repetitive stones are open
-# If the defender does not block the other, end, Attacker is guarenteed to win next turn.
-Closed4 = [
-    "xoooo-"
-    "-oooox"
-]
-# -----------------------------------------
 # (Reference)
 # I have also added Open2 for heuristics calculation.
 # However, they pose of low value, but, multiple Open2s can turn into a Open3xOpen3.
@@ -38,3 +16,39 @@ Open2 = [
     "--o-o-",
     "--o-o-"
 ]
+Open2Val = 5
+# -----------------------------------------
+# Open3 is a threat in which all the ends of 3 repetitive stones are open
+# The third and fourth are also counted as Open3
+Open3 = [
+    "-ooo--",
+    "--ooo-",
+    "-o-oo-",
+    "-oo-o-",
+    "x-ooo-"
+]
+Open3Val = 50
+# -----------------------------------------
+# Open4 is a threat in which all the ends of 4 repetitive stones are open
+# If the attacker does not have an Closed4 or Open4 to make into 5, Attacker is guarenteed to win next turn
+Open4 = [
+    "-oooo-"
+]
+# -----------------------------------------
+# Open5 is win result.
+Open5 = [
+    "xooooo",
+    "ooooox",
+    "-ooooo",
+    "ooooo-",
+]
+Open5Val = 10000000
+# -----------------------------------------
+# Closed4 is a threat in which one of the ends of 4 repetitive stones are open
+# If the defender does not block the other, end, Attacker is guarenteed to win next turn.
+Closed4 = [
+    "xoooo-",
+    "-oooox",
+    "xoo-oo",
+]
+Closed4Val = 50
