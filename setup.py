69a9c705d9a5c4eb0f71f8240da977fbb839c048
Dashadower <tttllshin@gmail.com>
2017-07-15 20:33:00 +0900

    setup.py: Fixed bug regarding include_files where freezing would recursively refreeze entire directory entile maxrecursiondepth.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys,requests,glob,os
 from cx_Freeze import setup, Executable
 os.environ['TCL_LIBRARY'] = r'C:\Python36\tcl\tcl8.6'
 os.environ['TK_LIBRARY'] = r'C:\\Python36\tcl\tk8.6'
-build_exe_options = {"packages":[],"include_files":[("C:/Python36/DLLs","tk86t.dll"),("C:/Python36/DLLs","tcl86t.dll"),(os.getcwd(),"settings.config")]}
+build_exe_options = {"packages":[],"include_files":[("C:/Python36/DLLs/tk86t.dll","tk86t.dll"),("C:/Python36/DLLs/tcl86t.dll","tcl86t.dll"),"settings.config"]}
 setup(
     name = "Omok",
     version = "1.0",
